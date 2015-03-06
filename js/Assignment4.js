0c045de2eb6cba1425245fae7c3e786778d0642f
Joshh92 <joshh92@uab.edu>
2015-03-06 14:00:25 -0600

    Assignment4built
    Build the applications.

---
+++ js/Assignment4.js
@@ -0,0 +1,19 @@
+function MenuInput()
+{
+    if (document.getElementById("menu").value=="Show Area 1")
+    {
+        document.getElementById("Area1").style.visibility="visible"
+        document.getElementById("Area2").style.visibility="hidden"
+    }
+    else if (document.getElementById("menu").value=="Show Area 2")
+    {
+        document.getElementById("Area2").style.visibility="visible"
+        document.getElementById("Area1").style.visibility="hidden"
+    }
+    else
+    {
+        document.getElementById("Area1").style.visibility="hidden"
+        document.getElementById("Area2").style.visibility="hidden"
+    }
+    
+}
